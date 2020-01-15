ed5346c68df421c4ab688c276966e8627a96cd6a
Natan <natan-moreira1@hotmail.com>
2020-01-14 21:57:18 -0300

    database config to prod

--- config/database.rb
+++ config/database.rb
@@ -19,3 +19,17 @@ configure :test do
       host: 'postgres'
     }
   end
+
+
+  configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/bot_production')
+   
+    set :database, {
+      adapter:  'postgresql',
+      host:     db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    }
+  end
